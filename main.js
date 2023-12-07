a1556b28d9af43ac5105a4729732147a94f5a032
samuellvn <138622449+samuellvn@users.noreply.github.com>
2023-12-07 14:15:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas;
+var imagens;
+var object_detector;
+var Status="";
+var objects=[];
+
+function preload(){
+    imagens=""
+}
+function setup(){
+    canvas=createCanvas(640, 420);
+    canvas.center();
+    object_detector=ml5.objectDetector("cocossd", model_loaded);
+    document.getElementById("status").innerHTML="status: detectando objetos";
+}
+function model_loaded(){
+    console.log("modelo carregado");
+    Status=true;
+    object_detector.detect(imagens, got_results);
+}
+function got_results(error, results){
+    if(error==true){
+        console.error(error);
+    }   else{
+        console.log(results);
+        objects=results;
+    }
+}
+function draw(){
+    image(imagens, 0, 0, 640, 420);
+    if(Status!=" "){
+        for(var o=0; o<objects.length; o++){
+            document.getElementById("status").innerHTML="status: objeto detectado";
+            var precisao=floor(objects[o].confidence*100);
+            fill("blue");
+            text(objects[o].label+" "+precisao+"%", objects[o].x, objects[o].y);
+            noFill();
+            stroke("blue");
+            rect(objects[o].x, objects[o].y, objects[o].width,  objects[o].height);
+        }
+    }
+}
